573067c5e09165044ef11a398c24b98111a8eaaf
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2021-08-24 00:26:44 +0700

    feat(home): landing page for user

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,24 @@
 <template>
   <div class="home">
-    <BlogPost :post="welcomeScreen" />
-    <BlogPost
-      :post="post"
-      v-for="(post, index) in sampleBlogPost"
-      :key="index"
-    />
+    <BlogPost v-if="!user" :post="welcomeScreen" />
+    <BlogPost :post="post" v-for="(post, index) in sampleBlog" :key="index" />
     <div class="blog-card-wrap">
       <div class="container">
         <h3>View More Recent Blogs</h3>
         <div class="blog-cards">
           <BlogCard
             :post="post"
-            v-for="(post, index) in sampleBlogCards"
+            v-for="(post, index) in sampleCard"
             :key="index"
           />
         </div>
       </div>
     </div>
-    <div class="updates">
+    <div v-if="!user" class="updates">
       <div class="container">
-        <h2>never miss a post. Register for your free account today!</h2>
-        <router-link class="router-button" to="#">
-          Register for MPBlogs <Arrow class="arrow arrow-light" />
+        <h2>Never miss a post. Register for you free account today!</h2>
+        <router-link class="router-button" :to="{ name: 'Register' }"
+          >Register for MPBlogs <Arrow class="arrow arrow-light" />
         </router-link>
       </div>
     </div>
@@ -39,18 +35,18 @@ export default {
   components: {
     BlogPost,
     BlogCard,
-    Arrow
+    Arrow,
   },
   data() {
     return {
       welcomeScreen: {
         title: "Welcome!",
         blogPost:
-          "Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!",
+          "Weekly blog articles with all things programming including HTML, CSS, Javascript and more. Register today ti never miss a post",
         welcomeScreen: true,
         photo: "coding",
       },
-      sampleBlogPost: [
+      sampleBlog: [
         {
           title: "This is a Filler Title!",
           blogHTML: "This is a filler blog post tittle!",
@@ -62,29 +58,15 @@ export default {
           blogCoverPhoto: "designed-for-everyone",
         },
       ],
-      sampleBlogCards: [
-        {
-          blogTitle: "Blog Card #1",
-          blogCoverPhoto: "stock-1",
-          blogDate: "May 1, 2021",
-        },
-        {
-          blogTitle: "Blog Card #2",
-          blogCoverPhoto: "stock-2",
-          blogDate: "May 1, 2021",
-        },
-        {
-          blogTitle: "Blog Card #3",
-          blogCoverPhoto: "stock-3",
-          blogDate: "May 1, 2021",
-        },
-        {
-          blogTitle: "Blog Card #4",
-          blogCoverPhoto: "stock-4",
-          blogDate: "May 1, 2021",
-        },
-      ],
     };
+  },
+  computed: {
+    sampleCard() {
+      return this.$store.state.sampleCard;
+    },
+    user() {
+      return this.$store.state.user;
+    },
   },
 };
 </script>
@@ -105,9 +87,32 @@ export default {
     flex-direction: column;
     align-items: center;
 
-    @media (min-width:800px) {
+    @media (min-width: 800px) {
       padding: 125px 25px;
       flex-direction: row;
+    }
+
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+
+      @media (min-width: 800px) {
+        margin-left: auto;
+      }
+    }
+
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+
+      @media (min-width: 800px) {
+        text-align: initial;
+      }
     }
   }
 }
