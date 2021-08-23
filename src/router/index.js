6c2c12826ee4e4f7bce09b43184af7e7020e9244
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2021-08-24 00:29:35 +0700

    fix(routes): add new router for blogs, login, register, and forgot password

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home";
+import Blogs from "@/views/Blogs";
+import Login from "@/views/Login";
+import Register from "@/views/Register";
+import ForgotPassword from "@/views/ForgotPassword";
 
 Vue.use(VueRouter);
 
@@ -9,6 +13,41 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      title: "Home",
+    },
+  },
+  {
+    path: "/blogs",
+    name: "Blogs",
+    component: Blogs,
+    meta: {
+      title: "Blogs",
+    },
+  },
+  {
+    path: "/forgot-passowrd",
+    name: "ForgotPassword",
+    component: ForgotPassword,
+    meta: {
+      title: "ForgotPassword",
+    },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+    meta: {
+      title: "Login",
+    },
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+    meta: {
+      title: "Register",
+    },
   },
 ];
 
@@ -16,6 +55,11 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | MPBlogs`;
+  next();
 });
 
 export default router;
